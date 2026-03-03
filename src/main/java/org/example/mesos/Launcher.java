e5e85ab7269b33e3d4f6f0d6a0ef05f3c66ddd36
Filippo Orsijena <filippo.orsijena@gmail.com>
2026-03-03 17:37:13 +0100

    first commit

---
+++ src/main/java/org/example/mesos/Launcher.java
@@ -0,0 +1,10 @@
+package org.example.mesos;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        //Application.launch(HelloApplication.class, args);
+        System.out.println("Hello Application");
+    }
+}
