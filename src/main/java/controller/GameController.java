6c43d70c357a68c4d06cde2c92bf154443b16e65
Rocco Panizzi <rocco.panizzi@gmail.com>
2026-03-11 16:23:06 +0100

    feat: slim controller

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import model.GameModel;
+import model.board.OfferTile;
+import model.cards.Card;
+import model.player.Player;
+
+import java.util.List;
+
+
 public class GameController {
+    private GameModel gameModel;
+
+    public GameController(GameModel gameModel) {
+        this.gameModel = gameModel;
+    }
+
+    //functions to be called by client
+    public void startGame(List<Player> players) {} //da capire come viene creata la partita. Come il client ci interagisce
+
+    public void placeTotem(Player player, OfferTile offerTile) {
+        //delega gameModel.placeModel()
+    }
+    public void drawCard(Player player, Card card) {
+        //delega a gameModel.drawCard()
+    }
 }
