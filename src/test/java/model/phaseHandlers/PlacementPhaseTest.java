1a6e417df95ee690f4e4c4b9a5418f7c5e8771f4
Rocco Panizzi <rocco.panizzi@gmail.com>
2026-03-29 16:29:30 +0200

    test (phase handlers) : Action added

--- src/test/java/model/phaseHandlers/PlacementPhaseTest.java
+++ src/test/java/model/phaseHandlers/PlacementPhaseTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.mock;
