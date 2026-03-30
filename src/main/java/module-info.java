37375f773c1df34e6f92b5dfb0d3bf1522769717
Rocco Panizzi <rocco.panizzi@gmail.com>
2026-03-30 17:23:20 +0200

    Revert "resolve conflicts to merge with main"
    This reverts commit ed14414131e26f320266c7176d8a123c58eeec2e, reversing
    changes made to 0ece8a040b6b1ca923d4718149ce016487c81534.

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.mesos {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens org.example.mesos to javafx.fxml;
-    exports org.example.mesos;
-}
